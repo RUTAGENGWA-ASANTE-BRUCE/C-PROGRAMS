f663c555f9fc0cca8ebbe12d58107e3e6e5cbe45
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 14:21:26 +0200

    new changes

---
+++ ice_cream.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+     
+   int ice_cream_price=950;
+   int pocket_money=1000;
+   
+   printf("Do I have money to buy one ice cream? %d \n",ice_cream_price<pocket_money);
+   printf("Do I have money to buy two ice cream? %d \n",2*ice_cream_price<pocket_money);
+   }
