bc73beefadd7648f56207a1f810d98c0aa86e918
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 14:25:05 +0200

    new changes

---
+++ structures2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct{
+		float real;
+		float imag;
+}_COMPLEX;
+void swap(_COMPLEX a,_COMPLEX b){
+		_COMPLEX tmp;
+		
+		tmp=a;
+		a=b;
+		b=tmp;
+}
+void print(_COMPLEX a){
+		printf("(%f,%f)\n",a.real,a.imag);
+}
+	
+void main(){
+	_COMPLEX x={4.5,5.0},y={10.0,15.0};
+	print(x);
+	print(y);
+	swap(x,y);
+	print(x);
+	print(y);
+}
