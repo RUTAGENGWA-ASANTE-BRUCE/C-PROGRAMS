2e84585ea9dbd6fc01beb5b055838df9d331faa0
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 14:25:29 +0200

    new changes

---
+++ using_while.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	int nCalculate=1;
+	//set the while condition
+	while(nCalculate<=12){
+		printf("%d ",nCalculate);
+		nCalculate++;
+	}
+	printf("\n");
+	return 0;
+}
