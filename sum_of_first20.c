1e5120a82240360392e22973ef325f3f772b2cf1
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 14:25:17 +0200

    new changes

---
+++ sum_of_first20.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(void){
+	int nSum=0;
+	int iRow;
+	for(iRow=1;iRow<=20;iRow++){
+		
+		nSum+=iRow;
+		
+	}   
+		printf("\n the sum of first 20 natural numbers = ");
+		printf("sum = %d",nSum);
+	}
